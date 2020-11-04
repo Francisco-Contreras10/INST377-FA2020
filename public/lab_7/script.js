61fd94d5ae2405c5283b87aecc77eeaa1b80cc58
Francisco-Contreras10 <70776323+Francisco-Contreras10@users.noreply.github.com>
2020-11-04 13:54:04 -0500

    Update script.js

--- public/lab_7/script.js
+++ public/lab_7/script.js
@@ -16,89 +16,82 @@ function convertRestaurantsToCategories(restaurantList) {
 
 function makeYourOptionsObject(datapointsFromRestaurantsList) {
   // set your chart configuration here!
-  CanvasJS.addColorSet("customColorSet1", [
+  CanvasJS.addColorSet('greenShades', [
     // add an array of colors here https://canvasjs.com/docs/charts/chart-options/colorset/
+    '#2F4F4F',
+    '#008080',
+    '#2E8B57',
+    '#3CB371',
+    '#90EE90'
   ]);
 
   return {
     animationEnabled: true,
-    colorSet: "miscAdobe",
+    colorSet: 'greenShades',
     title: {
-      text: "Places To Eat Out In Future",
+      text: 'Places to Eat Out In Future'
     },
     axisX: {
       interval: 1,
-      labelFontSize: 12,
+      labelFontSize: 12
     },
     axisY2: {
-      interlacedColor: "rgba(1,77,101,.2)",
-      gridColor: "rgba(1,77,101,.1)",
-      title: "Restaurant By Category",
+      interlacedColor: 'rgba(1,77,101,.2)',
+      gridColor: 'rgba(1,77,101,.1)',
+      title: 'Restaurant By Category',
       labelFontSize: 12,
       scaleBreaks: {
-        customBreaks: [
-          {
-            startValue: 40,
-            endValue: 50,
-            color: "orange",
-          },
-          {
-            startValue: 85,
-            endValue: 100,
-            color: "orange",
-          },
-          {
-            startValue: 140,
-            endValue: 175,
-            color: "orange",
-          },
-        ],
-      },
-      // Add your scale breaks here https://canvasjs.com/docs/charts/chart-options/axisy/scale-breaks/custom-breaks/
+        customBreaks: [{
+          startValue:40,
+          endValue:50,
+          color:'orange'
+        },
+        {
+          startValue:85,
+          endValue:100,
+          color:'orange'
+        },
+        {
+          startValue: 140,
+          endValue: 175,
+          color: 'orange'
+        }]
+      } // Add your scale breaks here https://canvasjs.com/docs/charts/chart-options/axisy/scale-breaks/custom-breaks/
     },
-    data: [
-      {
-        type: "bar",
-        name: "restaurants",
-        axisYType: "secondary",
-        dataPoints: datapointsFromRestaurantsList,
-      },
-    ],
+    data: [{
+      type: 'bar',
+      name: 'restaurants',
+      axisYType: 'secondary',
+      dataPoints: datapointsFromRestaurantsList
+    }]
   };
 }
 
 function runThisWithResultsFromServer(jsonFromServer) {
-  console.log("jsonFromServer", jsonFromServer);
-  sessionStorage.setItem("restaurantList", JSON.stringify(jsonFromServer)); // don't mess with this, we need it to provide unit testing support
+  console.log('jsonFromServer', jsonFromServer);
+  sessionStorage.setItem('restaurantList', JSON.stringify(jsonFromServer)); // don't mess with this, we need it to provide unit testing support
   // Process your restaurants list
   // Make a configuration object for your chart
   // Instantiate your chart
-  CanvasJS.addColorSet("miscAdobe", [
-    "#4F61F7",
-    "#5DDDFC",
-    "#60E69F",
-    "#94FC5D",
-    "#F2E75A",
-  ]);
-  const dataPoints = convertRestaurantsToCategories(jsonFromServer);
-  const options = makeYourOptionsObject(dataPoints);
-  const chart = new CanvasJS.Chart("chartContainer", options);
+  const reorganizedData = convertRestaurantsToCategories(jsonFromServer);
+  const options = makeYourOptionsObject(reorganizedData);
+  const chart = new CanvasJS.Chart('chartContainer', options);
   chart.render();
-  $(window).on("resize", () => {
+  $(window).on('resize', () => {
     chart.render();
   });
 }
 
 // Leave lines 52-67 alone; do your work in the functions above
-document.body.addEventListener("submit", async (e) => {
+document.body.addEventListener('submit', async (e) => {
   e.preventDefault(); // this stops whatever the browser wanted to do itself.
   const form = $(e.target).serializeArray();
-  fetch("/api", {
-    method: "POST",
+  fetch('/api', {
+    method: 'POST',
     headers: {
-      "Content-Type": "application/json",
+      'Content-Type': 'application/json'
     },
-    body: JSON.stringify(form),
+    body: JSON.stringify(form)
   })
     .then((fromServer) => fromServer.json())
     .then((jsonFromServer) => runThisWithResultsFromServer(jsonFromServer))
