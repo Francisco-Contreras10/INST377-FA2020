7d9b4aba838b708e11a12b4453b4c1f34eecf7bc
Francisco-Contreras10 <70776323+Francisco-Contreras10@users.noreply.github.com>
2020-11-07 15:37:40 -0500

    Update script.js

---
+++ public/asst-2/script.js
@@ -0,0 +1,13 @@
+
+const restaurant = [];
+fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json')
+  .then(response => response.json())
+  .then(data => restaurant.push(...data))
+
+  function findMatches(wordToMatch, restaurant){
+    return restaurant.filter(place => {
+        const regex = new RegExp(wordToMatch, 'gi');
+        return place.name.match(regex) || place.city.match(regex)
+    });
+
+}
