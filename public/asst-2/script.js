f5681e3c3beb4c9085bed8160da54f5be81f559c
Francisco-Contreras10 <70776323+Francisco-Contreras10@users.noreply.github.com>
2020-11-08 17:07:52 -0500

    Update script.js

--- public/asst-2/script.js
+++ public/asst-2/script.js
@@ -1,12 +1,42 @@
+
 const restaurant = [];
+
 fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json')
   .then(response => response.json())
-  .then(data => restaurant.push(...data))
+  .then(data => restaurant.push(...data));
+ 
 
   function findMatches(wordToMatch, restaurant){
     return restaurant.filter(place => {
         const regex = new RegExp(wordToMatch, 'gi');
-        return place.name.match(regex) || place.city.match(regex)
-    });
+        return place.name.match(regex) || place.city.match(regex);
+    })
+
 }
 
+function displayArray() {
+    return restaurant;
+}
+
+function displayMatches() {
+    const matchArray = findMatches(this.value, restaurant);
+    const html = matchArray.map(place => {
+        return `
+            <li>
+                <p class="nameLabel">${place.name}</p>
+                <p>Address: ${place.address_line_1}</p>
+                <p>City: ${place.city}</p>
+                <p>Category: ${place.category}</p>
+                <p>Proper Hand Washing: ${place.proper_hand_washing}</p>
+            </li>
+            `
+    }).join('');
+    suggestions.innerHTML = html;
+    
+}
+
+const searchInput = document.querySelector('.textentry');
+const suggestions = document.querySelector('.suggestions');
+
+searchInput.addEventListener('change', displayMatches);
+searchInput.addEventListener('keyup', displayMatches);
